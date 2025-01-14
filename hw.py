512376f9bc09531eb38d0b27138d1d5573750db2
Arashad2024 <ahm.rashad.95@gmail.com>
2025-01-14 05:15:00 +0200

    adding env variable and using os.environ to access it

--- hw.py
+++ hw.py
@@ -1,8 +1,12 @@
 import sys
+import os
 
 args = sys.argv
 arg = args[1]
 prog_file = args[0]
 
 print(f'Hello {arg} from {prog_file}')
+
+host = os.environ.get('HOST')
+print(f'Connecting to {host}')
 
