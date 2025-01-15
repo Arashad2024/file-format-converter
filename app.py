6a208a5baf5428b910584036900e633465400d64
Arashad2024 <ahm.rashad.95@gmail.com>
2025-01-15 06:12:54 +0200

    finalzing with exception handling

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+import sys
 import glob
 import os
 import json
 import re
 import pandas as pd
+from dotenv import load_dotenv
 
 def get_column_names(schemas, ds_name, sorting_key='column_position'):
     column_details = schemas[ds_name]
@@ -28,6 +30,8 @@ def to_json(df, tgt_base_dir, ds_name, file_name):
 def file_converter(src_base_dir, tgt_base_dir, ds_name):
     schemas = json.load(open(f'{src_base_dir}/schemas.json'))
     files = glob.glob(f'{src_base_dir}/{ds_name}/part-*')
+    if len(files) == 0:
+        raise NameError(f'No files found for {ds_name}')
 
     for file in files:
         df = read_csv(file, schemas)
@@ -35,15 +39,22 @@ def file_converter(src_base_dir, tgt_base_dir, ds_name):
         to_json(df, tgt_base_dir, ds_name, file_name)
 
 def processing_files(ds_names = None):
-    src_base_dir = os.environ.get('SRC_BASE_DIR')
-    tgt_base_dir = os.environ.get('TGT_BASE_DIR')
+    src_base_dir = os.getenv("SRC_BASE_DIR") #os.environ.get('SRC_BASE_DIR')
+    tgt_base_dir = os.getenv("TGT_BASE_DIR") #os.environ.get('TGT_BASE_DIR')
     schemas = json.load(open(f'{src_base_dir}/schemas.json'))
     if not ds_names:
         ds_names = schemas.keys()
     for ds_name in ds_names:
-        print(f'Processing {ds_name}')
-        file_converter(src_base_dir, tgt_base_dir, ds_name)
-
+        try:
+            print(f'Processing {ds_name}')
+            file_converter(src_base_dir, tgt_base_dir, ds_name)
+        except NameError as ne:
+            print(f'Error processing {ds_name}')
+            pass
 
 if __name__ == '__main__':
-    processing_files()
+    if len(sys.argv) == 2:
+        ds_names = json.loads(sys.argv[1])
+        processing_files(ds_names)
+    else:
+        processing_files()
